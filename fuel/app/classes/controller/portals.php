28228bda7184723862f3bacf0095b9bf23828b23
yamauchi <k.yamauchi@core-tech.jp>
2013-03-13 15:21:44 +0900

    ルートをportalsに変更

--- fuel/app/classes/controller/portals.php
+++ fuel/app/classes/controller/portals.php
@@ -17,7 +17,7 @@ class Controller_Portals extends Controller_Template {
 		$data['test'] = 'testtest';
 		$view = View::forge('portals/index', $data);
 
-		$this->template->title = 'portal';
+		$this->template->title = 'portals';
 		$this->template->content = $view;
 
 	}
