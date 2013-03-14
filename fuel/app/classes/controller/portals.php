6c4b97e198d58406028e05a3a9a985f481987c89
yamauchi <k.yamauchi@core-tech.jp>
2013-03-14 12:05:40 +0900

    test5

--- fuel/app/classes/controller/portals.php
+++ fuel/app/classes/controller/portals.php
@@ -20,7 +20,7 @@ class Controller_Portals extends Controller_Template {
 		// $data['uri'] = DOCROOT . 'assets/resourecs';
 		// 「/home/marutoto/www/yamauaa/portals/public/assets/resourecs」
 
-		if($handle = opendir(DOCROOT.'assets/resourecs')) {
+		if($handle = opendir(DOCROOT.'assets/resources')) {
 
 			while(false !== ($file = readdir($handle))) {
 				$data['file'] = $file;
