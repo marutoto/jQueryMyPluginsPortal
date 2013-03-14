8e8a9b58572a45fdabab7e4c80a20ff50382303c
yamauchi <k.yamauchi@core-tech.jp>
2013-03-14 12:22:17 +0900

    test7

--- fuel/app/views/portals/index.php
+++ fuel/app/views/portals/index.php
@@ -1,12 +1,12 @@
 
-<?php foreach($dirs as $dir => $dir_name) :?>
+<?php /*foreach($dirs as $dir => $dir_name) :?>
 
 	<div class="item">
 		<?php echo Html::anchor($dir, $dir_name);?>
 	</div>
 
-<?php endforeach;?>
-
+<?php endforeach;*/?>
+<?php echo $path;?>
 <div class="item">
 	<?php echo Html::anchor('#', 'under construction...');?>
 </div>
