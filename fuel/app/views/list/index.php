56effa89c24c6144ffa1ccc1a47efb57bfaf8eeb
yamauchi <k.yamauchi@core-tech.jp>
2013-03-14 18:24:42 +0900

    test53

--- fuel/app/views/list/index.php
+++ fuel/app/views/list/index.php
@@ -1,14 +1,37 @@
 
 <?php foreach($dirs as $dir) :?>
+	<div class="item-area">
 
-	<div class="item dir">
-		<?php echo $dir['dir_name'];?>
+		<div class="item dir">
+			<?php echo $dir['dir_name'];?>
+		</div>
+
+		<?php foreach($dir['files'] as $file) :?>
+
+			<?php echo Form::open()?>
+			<div class="item file">
+				<div class="left">
+					<?php echo Form::hidden('file_name', $file['file_name'])?>
+					<?php echo Form::hidden('file_path', $file['file_path'])?>
+					<?php echo $file['file_name'];?>
+				</div>
+				<div class="right">
+					<?php echo Form::submit('submit', 'Download')?>
+				</div>
+			</div>
+			<?php echo Form::close();?>
+
+		<?php endforeach;?>
+
 	</div>
+<?php endforeach;?>
 
-	<?php foreach($dir['files'] as $file) :?>
+
+<?php foreach($files as $file) :?>
+	<div class="item-area">
 
 		<?php echo Form::open()?>
-		<div class="item file">
+		<div class="item">
 			<div class="left">
 				<?php echo Form::hidden('file_name', $file['file_name'])?>
 				<?php echo Form::hidden('file_path', $file['file_path'])?>
@@ -20,25 +43,7 @@
 		</div>
 		<?php echo Form::close();?>
 
-	<?php endforeach;?>
-
-<?php endforeach;?>
-
-
-<?php foreach($files as $file) :?>
-
-	<?php echo Form::open()?>
-	<div class="item">
-		<div class="left">
-			<?php echo Form::hidden('file_name', $file['file_name'])?>
-			<?php echo Form::hidden('file_path', $file['file_path'])?>
-			<?php echo $file['file_name'];?>
-		</div>
-		<div class="right">
-			<?php echo Form::submit('submit', 'Download')?>
-		</div>
 	</div>
-	<?php echo Form::close();?>
 
 <?php endforeach;?>
 
